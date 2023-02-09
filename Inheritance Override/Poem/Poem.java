505211937ed78026afb9aabb61b87ced6bdb0620
Denielle Sumayao <111744814+DenielleSumayao@users.noreply.github.com>
2023-02-09 14:50:56 +0800

    Create Poem.java

---
+++ Inheritance Override/Poem/Poem.java
@@ -0,0 +1,18 @@
+package Poem;
+
+public class Poem {
+    String title;
+    int lines;
+    
+    public Poem(String title, int lines){
+    this.title = title;
+    this.lines = lines;
+}
+    public String getTitle(){
+        return this.title;
+    }
+    
+    public int getLines(){
+        return this.lines;
+    }
+}
